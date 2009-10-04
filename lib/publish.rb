489942934deeb7097ba7c9ea0b4e3452205ef7ac
Liam Clancy (metafeather) <metafeather@mac.com>
2009-10-04 14:36:29 +0100

    Rake based publishing into required gh-pages branch

---
+++ lib/publish.rb
@@ -0,0 +1,17 @@
+# Github requires the documentation to be in a special branch called 'gh-pages'
+# ref: http://judofyr.net/posts/copy-folders-to-a-branch.html    
+desc 'Publish the website to Github'
+task "publish" do
+  require 'grancher'
+  # Uses library version so that rake -T and rake gems:install do not fail if gem is missing
+  grancher = Grancher.new do |g|
+    g.directory 'website'
+
+    g.branch = 'gh-pages'
+    g.push_to = 'origin' # automatically push too    
+    g.message = 'Updated website'
+  end
+  grancher.commit
+  grancher.push
+end
+
